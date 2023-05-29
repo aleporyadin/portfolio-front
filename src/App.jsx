4def0e186a2dfae1309cc7f5dc3c6df96a5cedd7
Oleksandr Poriadin <ale.poryadin@gmail.com>
2023-05-29 18:00:02 +0300

    Deploy settings

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./provider/AuthProvider";
 import RouteSwitch from "./routes/RouteSwitch";
+import {env} from "../.eslintrc";
+
+console.log(env)
 
 export default function App() {
   return (
